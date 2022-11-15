1493abaa69d09cc59171715420822ab0ab143124
Nishimura44tky <83286066+Nishimura44tky@users.noreply.github.com>
2022-11-15 10:15:17 +0900

    Initial Commit

---
+++ MyMapBox/MyMapBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapBoxApp.swift
+//  MyMapBox
+//
+//  Created by 西村拓也 on 2022/11/15.
+//
+
+import SwiftUI
+
+@main
+struct MyMapBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
